4aa3391ad5254b24c9424568dc1a39b8ef470d5c
mclim-rsa25545 <martin.lim@rsa.rohde-schwarz.com>
2025-03-04 12:36:50 -0800

    remove data dir

--- test/__init__.py
+++ test/__init__.py
@@ -2,6 +2,8 @@ import sys
 from pathlib import Path
 
 # Get the path to the adjacent directory
-adjacent_dir = Path(__file__).parent.parent / 'EVM_Sweeps'
-sys.path.insert(0, str(adjacent_dir))       # Add it to sys.path
-# sys.path.remove(str(adjacent_dir))        # Remove the path
+adj_dir = Path(__file__).parent.parent / 'EVM_Sweeps'
+print(adj_dir)
+# sys.path.insert(0, str(adj_dir))        # Add it to sys.path
+sys.path.append(str(adj_dir))           # Add it to sys.path
+# sys.path.remove(str(adj_dir))         # Remove the path
