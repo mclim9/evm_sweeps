6b997e37401e25c5b311a59efa6794236ca7b100
mclim-rsa27637 <martin.lim@rsa.rohde-schwarz.com>
2026-05-19 15:21:14 -0700

    clean up

--- src/driver/VSG_util.py
+++ src/driver/VSG_util.py
@@ -1,12 +1,12 @@
 
 def set_SMW_K575(self, mode):                                   # K575 Mode
-    meas.VSG.write(':SOUR1:CORR:OPT:RF:IQM 1')              # Optimize IQ
-    meas.VSG.write(':SOUR1:CORR:OPT:RF:HEAD 1')             # Optimize Headroom
+    meas.VSG.write(':SOUR1:CORR:OPT:RF:IQM 1')                  # Optimize IQ
+    meas.VSG.write(':SOUR1:CORR:OPT:RF:HEAD 1')                 # Optimize Headroom
     if 'OFF' in mode:
-        meas.VSG.write(':SOUR1:CORR:OPT:RF:LIN OFF')        # Linearization Mode  | OFF | AUTO | MAN
+        meas.VSG.write(':SOUR1:CORR:OPT:RF:LIN OFF')            # Linearization Mode  | OFF | AUTO | MAN
     elif 'ON' in mode:
-        # meas.VSG.query(':SOUR1:CORR:OPT:RF:LIN:ADJ?')     # Linearize RF Button
-        meas.VSG.query(':SOUR1:CORR:OPT:RF:LIN AUTO;*OPC?') # Linearization Mode  | OFF | AUTO | MAN
+        # meas.VSG.query(':SOUR1:CORR:OPT:RF:LIN:ADJ?')         # Linearize RF Button
+        meas.VSG.query(':SOUR1:CORR:OPT:RF:LIN AUTO;*OPC?')     # Linearization Mode  | OFF | AUTO | MAN
     meas.VSG.query('*OPC?')
 
 
